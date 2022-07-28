69faed1b1a8805cb1e8bd06440e92340b854ea92
jazztinecruz <jzztn.crz@gmail.com>
2022-07-28 23:15:51 +0800

    Add testimonial section

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import About from '../components/sections/about'
 import Hero from '../components/sections/hero'
 import Layout from '../components/sections/layout'
 import Projects from '../components/sections/projects'
-import Testimonials from '../components/sections/testimonials'
+import Testimonials from '../components/sections/testimonial'
 
 const Home = () => {
   return (
