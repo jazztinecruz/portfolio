e8ab1c32e973e123a719e5f2ad4412e563cd4be8
jazztinecruz <jzztn.crz@gmail.com>
2022-07-28 22:35:10 +0800

    Add additional hompage sections

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
-import Layout from "../components/sections/layout"
+import About from '../components/sections/about'
+import Hero from '../components/sections/hero'
+import Layout from '../components/sections/layout'
+import Projects from '../components/sections/projects'
 
 const Home = () => {
   return (
-    <Layout>Hello</Layout>
+    <Layout>
+      <div className='grid gap-52'>
+        <Hero />
+        <Projects/>
+        <About />
+      </div>
+    </Layout>
   )
 }
 
