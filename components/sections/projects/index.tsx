4170e316ff3bc1f3857b323301652af9376344a8
jazztinecruz <jzztn.crz@gmail.com>
2022-07-28 22:35:30 +0800

    Add project component

---
+++ components/sections/projects/index.tsx
@@ -0,0 +1,20 @@
+import Project from "../../elements/project"
+import Title from "../../elements/section-title"
+
+const Projects = () => {
+  return (
+    <section id="#about" className="grid gap-32 lg:gap-40 items-center">
+      <Title title="Projects Showcase" />
+
+      <div className='flex flex-col gap-40 lg:gap-48'>
+        <Project/>
+        <Project/>
+        <Project/>
+        <Project/>
+        <Project/>
+      </div>
+    </section>
+  )
+}
+
+export default Projects
