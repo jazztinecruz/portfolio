75f372d89e4da03ac7b538e36e916bd7107e7c5a
jazztinecruz <jzztn.crz@gmail.com>
2022-07-30 22:10:16 +0800

    Change home button link

--- components/elements/nav-links/index.tsx
+++ components/elements/nav-links/index.tsx
@@ -7,7 +7,7 @@ interface Props {
 const NavLinks = ({ sidebar }: Props) => {
   return (
     <ul className={`${sidebar ? "flex flex-col items-center gap-4" : "flex items-center gap-6"}`}>
-      <NavLink sidebar={sidebar} title="Home"  link="#home"/>
+      <NavLink sidebar={sidebar} title="Home"  link="#hero"/>
       <NavLink sidebar={sidebar} title="Projects" link="#projects"/>
       <NavLink sidebar={sidebar} title="About Me" link="#about"/>
       <NavLink sidebar={sidebar} title="Testimonials" link="#testimonials"/>
