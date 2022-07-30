fac6842acfb27ce6b9bc4a5b0417c633bc831f0c
jazztinecruz <jzztn.crz@gmail.com>
2022-07-30 21:17:53 +0800

    Add style props

--- components/elements/footer-links/social-links/index.tsx
+++ components/elements/footer-links/social-links/index.tsx
@@ -3,11 +3,11 @@ import SocialLink from "./social-link"
 const SocialLinks = () => {
   return (
     <div className='flex items-center gap-6'>
-      <SocialLink image="/images/facebook.png"/>
-      <SocialLink image="/images/instagram.png"/>
-      <SocialLink image="/images/github.png"/>
-      <SocialLink image="/images/gmail.png"/>
-      <SocialLink image="/images/phone.png"/>
+      <SocialLink image="/images/facebook.png" style="w-6 h-8"/>
+      <SocialLink image="/images/instagram.png" style="w-6 h-6"/>
+      <SocialLink image="/images/github.png" style="w-6 h-6"/>
+      <SocialLink image="/images/gmail.png" style="w-8 h-6"/>
+      <SocialLink image="/images/phone.png" style="w-6 h-6"/>
     </div>
   )
 }
