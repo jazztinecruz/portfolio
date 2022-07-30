4a4613b0f3021d4d91b227413bd3afc2d6e71164
jazztinecruz <jzztn.crz@gmail.com>
2022-07-30 20:32:30 +0800

    Add id

--- components/sections/navbar/index.tsx
+++ components/sections/navbar/index.tsx
@@ -3,7 +3,7 @@ import NavLinks from '../../elements/nav-links'
 
 const Navbar = () => {
   return (
-    <nav className="grid grid-cols-[1fr,auto] items-center py-4 lg:py-8">
+    <nav id="home" className="grid grid-cols-[1fr,auto] items-center py-4 lg:py-8">
       <Logo />
       <NavLinks sidebar={false}/>
     </nav>
