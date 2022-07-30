0725e81481a2d0772382f3d9ed631dcfc0f35507
jazztinecruz <jzztn.crz@gmail.com>
2022-07-30 20:31:25 +0800

    Fix review card layout

--- components/elements/review/index.tsx
+++ components/elements/review/index.tsx
@@ -6,27 +6,27 @@ interface Props {
   name: string
   message: string
 }
-const Review = ({name, message}:Props) => {
+const Review = ({ name, message }: Props) => {
   return (
-    <div className="relative border-2 w-full h-full border-white p-6 flex flex-col gap-6 rounded-md">
+    <div className="relative border-2 w-full h-full border-white p-6 grid grid-rows-[auto,1fr] gap-6 rounded-md">
       {/* author and rate */}
       <div className="flex items-center gap-6">
         <Profile initial={name[0]} />
         <div className="flex flex-col gap-1">
-          <span className="font-semibold lg:text-lg tracking-wide">
-            {name}
-          </span>
+          <span className="font-semibold lg:text-lg tracking-wide">{name}</span>
           <Rate />
         </div>
       </div>
 
       {/* message */}
-      <p className="leading-relaxed tracking-wide text-center">
-        {message}
-      </p>
+      <div className='grid items-center'>
+        <p className="leading-relaxed tracking-wide text-center">
+          {message}
+        </p>
+      </div>
 
       {/* clip */}
-      <div className='absolute -top-10 -left-6 lg:-left-8'>
+      <div className="absolute -top-10 -left-6 lg:-left-8">
         <div className="relative w-[59px] h-[59px] lg:w-[69px] lg:h-[69px]">
           <Image src="/images/clip.png" layout="fill" />
         </div>
