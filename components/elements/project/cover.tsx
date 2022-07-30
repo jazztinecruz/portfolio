bf9d04923cf4758ae2cf24a41d0f1b472f332344
jazztinecruz <jzztn.crz@gmail.com>
2022-07-30 21:39:10 +0800

    Fix image width

--- components/elements/project/cover.tsx
+++ components/elements/project/cover.tsx
@@ -2,9 +2,9 @@ import Image from "next/image"
 
 const Cover = () => {
   return (
-    <div className='relative'>
+    <div className='border-4 relative w-full'>
       {/* image */}
-      <div className='w-full lg:w-96 h-64 bg-gray-200'></div>
+      <div className='w-full lg:w-96 h-64 bg-gray-300'></div>
 
       {/* left grid dot */}
       <div className='w-[100px] h-[100px] absolute -top-8 -left-2 lg:-top-2 lg:-left-16 -z-50'>
