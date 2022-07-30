3ded4f1179119760872c6c6ebaa89edeb54e16d5
jazztinecruz <jzztn.crz@gmail.com>
2022-07-30 20:33:10 +0800

    Add technologies section on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Hero from '../components/sections/hero'
 import Layout from '../components/layout'
 import Projects from '../components/sections/projects'
 import Testimonials from '../components/sections/testimonials'
+import Technologies from '../components/sections/technologies'
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
         <Projects/>
         <About />
         <Testimonials/>
+        <Technologies/>
       </div>
     </Layout>
   )
