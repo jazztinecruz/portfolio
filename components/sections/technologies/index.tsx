9ce61e7a19163a8727513a9e04285c08ca63aa9e
jazztinecruz <jzztn.crz@gmail.com>
2022-07-30 21:17:13 +0800

    Add images style

--- components/sections/technologies/index.tsx
+++ components/sections/technologies/index.tsx
@@ -12,23 +12,23 @@ const Technologies = () => {
       <div className="grid gap-12 lg:gap-20">
         {/* html, css, ts, js */}
         <div className="grid grid-flow-col lg:justify-center lg:gap-24 items-center">
-          <Techonology image="/images/html.png" title="HTML" />
-          <Techonology image="/images/css.svg.png" title="CSS" />
-          <Techonology image="/images/js.png" title="JavaScript" />
-          <Techonology image="/images/ts.svg.png" title="TypeScript" />
+          <Techonology image="/images/html.png" title="HTML" style="w-20 h-20 lg:w-[160px] lg:h-[160px]"/>
+          <Techonology image="/images/css.svg.png" title="CSS" style="w-20 h-20 lg:w-[160px] lg:h-[160px]"/>
+          <Techonology image="/images/js.png" title="JavaScript" style="w-20 h-20 lg:w-[160px] lg:h-[160px]"/>
+          <Techonology image="/images/ts.svg.png" title="TypeScript" style="w-20 h-20 lg:w-[160px] lg:h-[160px]"/>
         </div>
 
         {/* react, nextjs, tailwind */}
         <div className="grid grid-flow-col lg:justify-center lg:gap-24 items-center">
-          <Techonology image="/images/react.svg.png" title="React" />
-          <Techonology image="/images/nextjs.png" title="NextJs" />
-          <Techonology image="/images/tailwind.svg.png" title="TailwindCSS" />
+          <Techonology image="/images/react.svg.png" title="React"  style="w-24 h-20 lg:w-[180px] lg:h-[160px]" />
+          <Techonology image="/images/nextjs.png" title="NextJs" style="w-32 h-20 lg:w-[450px] lg:h-[250px]" />
+          <Techonology image="/images/tailwind.svg.png" title="TailwindCSS" style="w-20 h-20 lg:w-[160px] lg:h-[160px]" />
         </div>
 
         {/* sass, figma, */}
         <div className="grid grid-flow-col lg:justify-center lg:gap-24 items-center">
-          <Techonology image="/images/sass.svg.png" title="Sass" />
-          <Techonology image="/images/figma.png" title="Figma" />
+          <Techonology image="/images/sass.svg.png" title="Sass" style="w-20 h-20 lg:w-[180px] lg:h-[160px]"/>
+          <Techonology image="/images/figma.png" title="Figma" style="w-20 h-28 lg:w-[130px] lg:h-[180px]"/>
         </div>
       </div>
     </section>
