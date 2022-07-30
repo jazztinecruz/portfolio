232fc262d1d9f800057a8b73de45c6368e217f3f
jazztinecruz <jzztn.crz@gmail.com>
2022-07-30 22:05:20 +0800

    Fix facebook image

--- components/elements/social-links/index.tsx
+++ components/elements/social-links/index.tsx
@@ -3,7 +3,7 @@ import SocialLink from "./social-link"
 const SocialLinks = () => {
   return (
     <div className='flex items-center gap-6'>
-      <SocialLink image="/images/facebook.png" style="w-6 h-8"/>
+      <SocialLink image="/images/facebook.png" style="w-5 h-7"/>
       <SocialLink image="/images/instagram.png" style="w-6 h-6"/>
       <SocialLink image="/images/github.png" style="w-6 h-6"/>
       <SocialLink image="/images/gmail.png" style="w-8 h-6"/>
