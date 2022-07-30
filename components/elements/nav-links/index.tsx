013e67d24c07d15b8aa8aa187baa5e13118f86e7
jazztinecruz <jzztn.crz@gmail.com>
2022-07-30 20:30:59 +0800

    Add id links

--- components/elements/nav-links/index.tsx
+++ components/elements/nav-links/index.tsx
@@ -7,11 +7,11 @@ interface Props {
 const NavLinks = ({ sidebar }: Props) => {
   return (
     <ul className={`${sidebar ? "flex flex-col items-center gap-4" : "flex items-center gap-6"}`}>
-      <NavLink sidebar={sidebar} title="Home" />
-      <NavLink sidebar={sidebar} title="Projects" />
-      <NavLink sidebar={sidebar} title="About Me" />
-      <NavLink sidebar={sidebar} title="Testimonials" />
-      <NavLink sidebar={sidebar} title="Contact Me" />
+      <NavLink sidebar={sidebar} title="Home"  link="#home"/>
+      <NavLink sidebar={sidebar} title="Projects" link="#projects"/>
+      <NavLink sidebar={sidebar} title="About Me" link="#about"/>
+      <NavLink sidebar={sidebar} title="Testimonials" link="#testimonials"/>
+      <NavLink sidebar={sidebar} title="Contact Me" link="#contacts"/>
     </ul>
   )
 }
--- components/elements/nav-links/nav-link.tsx
+++ components/elements/nav-links/nav-link.tsx
@@ -1,15 +1,26 @@
+import Link from 'next/link'
+
 interface Props {
   title: string
   sidebar: boolean
+  link: string
 }
 
-const NavLink = ({ title, sidebar }: Props) => {
+const NavLink = ({ title, sidebar, link }: Props) => {
   return (
-    <li className={`${sidebar ? "text-secondary hover:bg-secondary hover:text-white w-full py-3 text-center rounded-md" : "opacity-60 hover:opacity-100"} transition-all duration-300 cursor-pointer`}>
-      <a className="text-sm lg:text-md font-semibold tracking-wide ">
-        {title}
-      </a>
-    </li>
+    <Link href={link}>
+      <li
+        className={`${
+          sidebar
+            ? 'text-secondary hover:bg-secondary hover:text-white w-full py-3 text-center rounded-md'
+            : 'opacity-60 hover:opacity-100'
+        } transition-all duration-300 cursor-pointer`}
+      >
+        <a className="text-sm lg:text-md font-semibold tracking-wide ">
+          {title}
+        </a>
+      </li>
+    </Link>
   )
 }
 
